579248274179b95873efa239e1344d2041a17596
AndrewYKim <anykim128@gmail.com>
2018-06-09 22:52:28 -0700

    initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,23 @@
+
+
+public class BinarySearch {
+	
+	public static int binarySearch(int[] array, int key) {
+		int low = 0;
+		int high = array.length-1;
+		
+		while (low <= high) {
+			int mid = low + (high-low)/2; // average preventing overflow
+			
+			if (array[mid] < key) {
+				low = mid + 1;
+			} else if (array[mid] == key) {
+				return mid;
+			} else {
+				high = mid - 1;
+			}
+		}
+		
+		return -1; // 
+	}
+}
